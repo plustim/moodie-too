49cb1b70191919a6198ab56225f94c282005063f
plustim <tim@plustim.com>
2017-11-19 06:43:03 -0800

    testing image submission from heroku

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const base64ToImg = require('base64-to-image');
 const fs = require("fs");
+const https = require("https");
+https.post = require("https-post");
 
 const router = express.Router();
-
-// declare axios for making http requests
-const axios = require('axios');
-const API = 'https://api-us.faceplusplus.com/facepp/v3/detect';
-
 
 router.get('/', (req, res) => {
   res.send('api works');
@@ -15,12 +12,12 @@ router.get('/', (req, res) => {
 
 
 router.post('/judge', (req, res) => {
-	const baseURL = "https://moodietoo.herokuapp.com";
+	const baseURL = "https://moodie-too.herokuapp.com/";
 	const imageURI = req.body.face;
 
 	// convert this to an image face++ will understand (they say they can accept base64, but in my experience they can't)
-	const image = base64ToImg(imageURI, "dist/temp/", {debug: true});
-	//const imageUrl = baseURL + "/temp/" + image.fileName;
+	//const image = base64ToImg(imageURI, "dist/temp/", {debug: true});
+	const imageUrl = baseURL + "/temp/" + image.fileName;
 	const imageUrl = "http://faceresearch.org/uploads/base/white_male";
 	
 	const fppParams = {
@@ -31,17 +28,19 @@ router.post('/judge', (req, res) => {
 	};
 
 	// send link to the image to f++ for evaluation
-	axios.post(API, fppParams)
-	.then(data => {
-		var faceObj = JSON.parse(data);
-		if( faceObj.hasOwnProperty("faces") ){
-			res.status(200).json(faceObj.faces[0].attributes.emotion);
-		}else{
-			var feedback = {sadness: 0, neutral: 0, disgust: 0, anger: 0, surprise: 0, fear: 0, happiness: 0};
-			res.json(feedback);
-		};
-	})
-	.catch(error => {res.status(500).send(error)});
+	https.post("https://api-us.faceplusplus.com/facepp/v3/detect", fppParams, function(response){
+		response.setEncoding('utf8');
+		response.on('data', chunk => {
+			const faceObj = JSON.parse(chunk);
+			console.log("FACE: ", faceObj.faces[0].attributes.emotion);
+			if( faceObj.hasOwnProperty("faces") ){
+				res.status(200).json(faceObj.faces[0].attributes.emotion);
+			}else{
+				var feedback = {sadness: 0, neutral: 0, disgust: 0, anger: 0, surprise: 0, fear: 0, happiness: 0};
+				res.json(feedback);
+			};
+		});
+	});
 });
 
 module.exports = router;
